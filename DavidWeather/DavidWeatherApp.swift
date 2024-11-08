858de38d13851a1f8291e36e234f2afb7cb4b174
davidCaluag <caluagd@sheridancollege.ca>
2024-11-08 14:44:00 -0500

    Initial Commit

---
+++ DavidWeather/DavidWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  DavidWeatherApp.swift
+//  DavidWeather
+//
+//  Created by David C on 2024-11-08.
+//
+
+import SwiftUI
+
+@main
+struct DavidWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
